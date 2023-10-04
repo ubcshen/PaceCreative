5a27cdbb21539d2349e55c020a2e605b555e622e
Sam Shen <ubcshen@gmail.com>
2023-10-04 08:44:47 -0700

    Create functions.php
    Create a WordPress Hook that allows you to add social sharing links (Linkedin and
    Facebook) after the title in the single.php template.

---
+++ functions.php
@@ -0,0 +1,20 @@
+function add_fb_linkedin_social_sharing_links_after_title($title) {
+    if (is_single()) { 
+        $post_permalink = get_permalink(); 
+        $post_title = get_the_title(); 
+
+        $linkedin = 'https://www.linkedin.com/shareArticle?mini=true&url=' . urlencode($post_permalink);
+        $facebook = 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($post_permalink);
+
+        $social_links_container = '<div class="social-sharing-container">';
+        $social_links_container .= '<a href="' . $linkedin . '" target="_blank" rel="nofollow" id="linkedIn">LinkedIn</a>'; <-- or use some font icons
+        $social_links_container .= '<a href="' . $facebook . '" target="_blank" rel="nofollow" id="faceBook">Facebook</a>';
+        $social_links_container .= '</div>';
+
+        $title .= $social_links_container;
+    }
+
+    return $title;
+}
+
+add_filter('the_title', 'add_fb_linkedin_social_sharing_links_after_title');
